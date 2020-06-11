4f5888a9badfb6fc3c47196c58259a2687ca8607
Erick Reyes <erguzman96@gmail.com>
2020-06-11 14:43:28 -0500

    Adjusting error page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,8 +1,9 @@
 const Error404 = () => {
     const view = `
         <section class="error404">
-            <img src="https://i.imgur.com/JvZnbG5.png">
-            <h2>Error 404</h2>
+                <img src="https://i.imgur.com/JvZnbG5.png">
+                <h2>¡Pagina no encontrada!</h2>
+
         </section>
     `;
 
