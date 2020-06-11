ff30b4f0f5907f05ac42aa0e172d046b4a08e099
Erick Reyes <erguzman96@gmail.com>
2020-06-11 14:56:39 -0500

    Adjusting error page responsive

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,8 @@
 const Error404 = () => {
     const view = `
         <section class="error404">
-                <img src="https://i.imgur.com/JvZnbG5.png">
-                <h2>¡Pagina no encontrada!</h2>
-
+            <img src="https://i.imgur.com/JvZnbG5.png">
+            <h2>¡Page not found!</h2>
         </section>
     `;
 
